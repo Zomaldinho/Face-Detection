61551c40e1ca85032dc778180ac858d49426308a
Zoma <hazem_ahmed_93@yahoo.com>
2020-03-23 14:43:30 +0200

    making url text bar responsive

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -9,7 +9,7 @@ const ImageLinkForm = ({onInputChange, onSubmit}) => {
             </p>
             <div>
                 <div className='center pa4 br3 shadow-5 form'>
-                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} placeholder='Enter a photo URL'/>
                     <button className='w-30 grow f4 link ph3 pv2 dib white bg-silver' onClick={onSubmit}>Detect</button>
                 </div>   
             </div>
