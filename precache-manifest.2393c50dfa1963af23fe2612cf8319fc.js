fc4036803158ba729661c1d9687e436e659410bb
Zoma <hazem_ahmed_93@yahoo.com>
2020-04-25 22:36:37 +0200

    Updates

---
+++ precache-manifest.2393c50dfa1963af23fe2612cf8319fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0243c850ce2eebaf02a96a05f94f5f",
+    "url": "/Face-Detection/index.html"
+  },
+  {
+    "revision": "56e8356cb54ad3b281f4",
+    "url": "/Face-Detection/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "54d30508b91f47d1de08",
+    "url": "/Face-Detection/static/css/main.541f1354.chunk.css"
+  },
+  {
+    "revision": "56e8356cb54ad3b281f4",
+    "url": "/Face-Detection/static/js/2.f653d10d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Face-Detection/static/js/2.f653d10d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d30508b91f47d1de08",
+    "url": "/Face-Detection/static/js/main.cfe9d564.chunk.js"
+  },
+  {
+    "revision": "82f2ab45145361e299fb",
+    "url": "/Face-Detection/static/js/runtime-main.491392e3.js"
+  }
+]);
